3c220f31f9592840fd94ad48cbd9064a758c34eb
selmaar118-wq <selmaar118@gmail.com>
2026-05-16 16:24:29 -0400

    Change static file serving directory in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // rooms: { roomCode: { host: ws, guest: ws, hostName: str, guestName: str } }
 const rooms = {};
